2e6ff63259e611d9c485fcbaada82562d45ebf53
Bohao Wang <bj050323@gmail.com>
2022-05-18 22:24:04 +0800

    fix: suffix error

--- build.js
+++ build.js
@@ -1,59 +1,7 @@
 const path = require('path')
+const fs = require('fs')
 
-const getEntry = () => {
-  return `
-    import { loader as loader0 } from '${path.resolve('src/Greet.jsx')}'
-    import { loader as loader1 } from '${path.resolve('src/Hello.jsx')}'
-    Promise.all([
-      loader0(),
-      loader1(),
-    ]).then(results => {
-      console.log(results)
-    })
-  `
-}
-
-const reactShim = path.resolve(__dirname, "react.ts");
-
-require('esbuild').build({
-  stdin: {
-    contents: getEntry(),
-    // resolveDir: path.resolve('src')
-  },
-  platform: 'browser',
-  // inject: [reactShim],
-  format: 'esm',
-  mainFields: ['module', 'main'],
-  bundle: true,
-  sourcemap: false,
-  target: 'esnext',
-  outfile: 'dist/loader.js',
-  loader,
-  plugins: [
-    {
-      name: 'imports',
-      setup(build) {
-        let entry;
-        build.onResolve({ filter: /.*/ }, (args) => {
-          if (args.path.startsWith('.') || args.path.startsWith('/')) {
-            console.log('args.path', args.path);
-            return { path: path.resolve(args.resolveDir, args.path) + (args.path.startsWith('.') ? '.js' : '') };
-          }
-          return {
-            path:
-              !args.path.startsWith('.') && !args.path.startsWith('/')
-                ? args.path
-                : path.resolve(args.resolveDir, args.path),
-            external: true,
-            sideEffects: false,
-          };
-        });
-      },
-    }
-  ]
-}).catch(() => process.exit(1))
-
-var loader = {
+const loader = {
   '.aac': 'file',
   '.css': 'text',
   '.less': 'text',
@@ -84,4 +32,62 @@ var loader = {
   '.webp': 'file',
   '.woff': 'file',
   '.woff2': 'file',
-};
+}
+
+const entryExts = [".js", ".jsx", ".ts", ".tsx"]
+
+function findEntry(dir, basename) {
+  for (let ext of entryExts) {
+    let file = path.resolve(dir, basename + ext)
+    if (fs.existsSync(file)) return path.resolve(dir, file)
+  }
+  return undefined
+}
+
+const getEntry = () => {
+  return `
+    import { loader as loader0 } from '${path.resolve('src/Greet')}'
+    import { loader as loader1 } from '${path.resolve('src/Hello')}'
+    Promise.all([
+      loader0(),
+      loader1(),
+    ]).then(results => {
+      console.log(results)
+    })
+  `
+}
+
+
+require('esbuild').build({
+  stdin: {
+    contents: getEntry(),
+    // resolveDir: path.resolve('src')
+  },
+  platform: 'browser',
+  // inject: [reactShim],
+  format: 'esm',
+  mainFields: ['module', 'main'],
+  bundle: true,
+  sourcemap: false,
+  target: 'esnext',
+  outfile: 'dist/loader.js',
+  loader,
+  plugins: [
+    {
+      name: 'imports',
+      setup(build) {
+        build.onResolve({ filter: /.*/ }, (args) => {
+          if (args.path.startsWith('.') || args.path.startsWith('/')) {
+            return { path: findEntry(args.resolveDir, args.path) }
+          }
+
+          return {
+            path: path.resolve(args.resolveDir, args.path),
+            external: true,
+            sideEffects: false,
+          }
+        })
+      },
+    }
+  ]
+}).catch(() => process.exit(1))
